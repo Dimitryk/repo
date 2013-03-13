eaa83704c438947dafa7caff1d3936f157378515
Dimitry Kongevold <kogevold.dimi@gmail.com>
2013-03-13 12:43:17 +0100

    Deleting bad project

--- PointCloudCombine/PointCloudCombine/icp.h
+++
@@ -1,9 +0,0 @@
-
-#ifndef __icp_h_
-#define __icp_h_
-#include "vector.h"
-
-float* globalICPRegistration( float* model, int modelSize, Vector3f min, Vector3f max, float *data, int dataSize, int maxIterations);
-
-
-#endif
--- PointCloudCombine/PointCloudCombine/rPPoints.h
+++
@@ -1,7 +0,0 @@
-
-#include "gl3w.h"
-
-int openPLY_file(char*);
-long createVertexArray(GLfloat **);
-long createTriList(GLuint **);
-int read_PLY(void);
